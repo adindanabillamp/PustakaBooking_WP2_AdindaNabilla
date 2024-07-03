041608656eb7b57e254284384367ab1b25602855
adindanabillamp <adindanabilla55@gmail.com>
2024-07-03 22:57:27 +0700

    menyelesaikan pustaka booking

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+{
+    echo "<h1>Perkenalkan</h1>";
+    echo "Nama saya Adinda Nabilla, saya tinggal di Bekasi, Hobi saya adalah bermain game";
+}
+}
